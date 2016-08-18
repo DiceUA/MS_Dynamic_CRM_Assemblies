dfb58502c8a3e4f879a416f1b743014fc61fc9fd
DiceUA <pillar.katter@gmail.com>
2016-08-18 23:23:33 +0300

    HW 3

---
+++ Education.Babikov.Plugins/GetStoreManager.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Query;
+using System.Activities;
+using Microsoft.Xrm.Sdk.Workflow;
+
+namespace Education.Babikov.Plugins
+{
+    public class GetStoreManager : CodeActivity
+    {
+        // Declare parameters
+        [RequiredArgument]
+        [Input("Store Reference")]
+        [ReferenceTarget("new_store_babikov")]
+        public InArgument<EntityReference> StoreRef { get; set; }
+        [Output("Contact Reference")]
+        [ReferenceTarget("contact")]
+        public OutArgument<EntityReference> ContactRef { get; set; }
+
+
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            IWorkflowContext workflowContext = context.GetExtension<IWorkflowContext>();
+            IOrganizationServiceFactory factory = context.GetExtension<IOrganizationServiceFactory>();
+            IOrganizationService service = factory.CreateOrganizationService(workflowContext.UserId);
+            // Debug Log ? Will it work here?
+            //ITracingService trace = context.GetExtension<ITracingService>();
+
+            EntityReference storeRef = StoreRef.Get(context);            
+
+            if(storeRef == null)
+            {
+                throw new InvalidPluginExecutionException("Store not found in workflow settings");
+            }
+
+            // Get all contacts attached to store
+            QueryExpression query = new QueryExpression("contact")
+            {
+                // preferredcontactmethodcode - optionset
+                // where email value = 2
+                // emailaddress1 - string
+                ColumnSet = new ColumnSet("emailaddress1", "preferredcontactmethodcode"),
+                LinkEntities =
+                {
+                    new LinkEntity("contact","new_store_babikov_contact","contactid","contactid", JoinOperator.Inner)
+                    {
+                        LinkEntities =
+                        {
+                            new LinkEntity("new_store_babikov_contact","new_store_babikov","new_store_babikovid","new_store_babikovid", JoinOperator.Inner)
+                            {
+                                LinkCriteria = new FilterExpression(LogicalOperator.And)
+                                {
+                                    Conditions =
+                                    {
+                                        new ConditionExpression("new_store_babikovid",ConditionOperator.Equal, storeRef.Id)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            IReadOnlyCollection<Entity> contacts = service.RetrieveMultiple(query).Entities;
+            if(contacts.Count > 0)
+            {
+                foreach (var item in contacts)
+                {
+                    OptionSetValue contactMethod = item.GetAttributeValue<OptionSetValue>("preferredcontactmethodcode");
+                    string email = item.GetAttributeValue<string>("emailaddress1");
+                    
+                    // Email must be filled, Preffered contact method must be Email = 2
+                    if (contactMethod != null && contactMethod.Value == 2 && !String.IsNullOrWhiteSpace(email))
+                    {
+                        // Return that contact reference
+                        ContactRef.Set(context,
+                            new EntityReference()
+                            {
+                                Id = item.Id,
+                                LogicalName = item.LogicalName
+                            });
+                        return; // and exit
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Education.Babikov.Plugins/HandleOrderStatusChange.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace Education.Babikov.Plugins
+{
+    public class HandleOrderStatusChange : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            // Connection
+            IOrganizationServiceFactory factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IOrganizationService service = factory.CreateOrganizationService(context.UserId);
+            // Debug.Log
+            ITracingService trace = serviceProvider.GetService(typeof(ITracingService)) as ITracingService;
+
+            trace.Trace("Action Plugin initialized!");
+
+            if (String.Equals("new_Action_OrderStatusChange_babikov", context.MessageName, StringComparison.OrdinalIgnoreCase) &&
+                context.Stage == 40)
+            {
+                // Here we can make changes
+                // For example
+
+                // Access input parameters
+                string subject = context.InputParameters["Subject"] as string;
+                //int? body = (int?)context.InputParameters["Body"];
+                subject = " (ActionPlugin)";
+                //body += 33;
+
+                // Update input parameters
+                context.InputParameters["Subject"] = subject;
+                //context.InputParameters["Body"] = body;
+
+                // Update Output parameters
+                //context.OutputParameters["Contact"] = new EntityReference("contact", Guid.NewGuid());
+            }
+        }
+    }
+}
